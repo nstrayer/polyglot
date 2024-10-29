42480a902a1dee63b7176071714ee36ed89667f8
Nick Strayer <nick.strayer@rstudio.com>
2024-10-29 12:44:49 -0400

    Initial commit from Create Next App

---
+++ app/api/use-chat-streamdata/route.ts
@@ -0,0 +1,32 @@
+import { openai } from '@ai-sdk/openai';
+import { generateId, StreamData, streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  // use stream data
+  const data = new StreamData();
+  data.append('initialized call');
+
+  const result = await streamText({
+    model: openai('gpt-4o'),
+    messages,
+    onFinish() {
+      // message annotation:
+      data.appendMessageAnnotation({
+        id: generateId(), // e.g. id from saved DB record
+        other: 'information',
+      });
+
+      // call annotation:
+      data.append('call completed');
+
+      data.close();
+    },
+  });
+
+  return result.toDataStreamResponse({ data });
+}
